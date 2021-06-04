18d71f538ba245831453c76163c3fd94cfc61324
sheeshalicia <maenadish@gmail.com>
2021-06-04 09:36:44 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+var themeSwitch = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123K";
+});
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47K";
+});
+pets.addEventListener("mouseover", function () {
+  pets.innerText = "20K";
+});
+
+themeSwitch.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    h1.innerText = "Stealth Quincy";
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    h1.innerText = "Party Quincy";
+  }
+});
